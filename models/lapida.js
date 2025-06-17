7144becdae61fd85bbf3dd732c87c8ae35c7019d
Mafer0610 <maria.cruz74@unach.mx~>
2025-06-17 12:29:50 -0600

    base

--- models/lapida.js
+++ models/lapida.js
@@ -3,11 +3,23 @@ const mongoose = require("mongoose");
 const LapidaSchema = new mongoose.Schema({
     NOM_REG: String,
     NOMBRE_PROPIE: String,
+    DIRECCION: String,
+    UBICACION: String,
     LOTE: String,
+    MEDIDAS_NO: String,
     EDO_CONTRI: String,
+    COLONIA: String,
     MEDIDAS: String,
+    LOTES_A: String,
+    ZONA: String,
+    FILA: String,
+    RUTA: String,
     X: String,
     Y: String,
+    CVE_POB: String,
+    CVE_PANTEO: String,
+    CVE_ZONA: String,
+    CVE_LOTE: String,
     CUENTA: String
 });
 
